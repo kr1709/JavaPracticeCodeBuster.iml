7a4da46f649ea8bfa482e3612cc38ff0cfde8957
kr1709 <kirujan17@gmail.com>
2024-11-10 01:16:12 +0000

    Initial commit

---
+++ javaDemo/src/homework/Homework.java
@@ -0,0 +1,264 @@
+package homework;
+
+public class Homework {
+    public static void main(String[] args) {
+        int val1;
+        int val2;
+        int total;
+        String text;
+        Double doubleVal1;
+/*
+//Question 1
+        System.out.println("Question 1");
+        String fullName = "Kirujan ravichandran";
+        int age = 29;
+        String location = "London";
+
+        System.out.println("Name = " + fullName);
+        System.out.println("Age = " + age);
+        System.out.println("Location = " + location);
+        System.out.println();
+
+//Question 2
+        System.out.println("Question 2");
+        System.out.println(" +\" \" \" \" \" +");
+        System.out.println("[|   o   o  |]"  );
+        System.out.println(" |     ^    |");
+        System.out.println(" |    '_'   |");
+        System.out.println(" +__________+");
+        System.out.println();
+
+//Question 3
+        System.out.println("Question 3");
+        System.out.println("**        **    *********");
+        System.out.println("**      **      **      **");
+        System.out.println("**    **        **      **");
+        System.out.println("**  **          **      **");
+        System.out.println("****            *********");
+        System.out.println("**  **          ****");
+        System.out.println("**    **        **  **");
+        System.out.println("**      **      **    **");
+        System.out.println("**        **    **      **");
+        System.out.println();
+
+//Question 4
+        System.out.println("Question 4");
+        System.out.println("*");
+        System.out.println("**");
+        System.out.println("***");
+        System.out.println("****");
+        System.out.println("*****");
+        System.out.println("******");
+        System.out.println();
+
+//Question 5
+        System.out.println("Question 5");
+        System.out.println("     *  ");
+        System.out.println("    * * ");
+        System.out.println("   * * *");
+        System.out.println("  * * * *");
+        System.out.println(" * * * * *");
+        System.out.println("* * * * * *");
+        System.out.println();
+
+//Question 6
+        System.out.println("Question 6");
+        System.out.println("* * * * * * ======================================");
+        System.out.println(" * * * * * =======================================");
+        System.out.println("* * * * * * ======================================");
+        System.out.println(" * * * * * =======================================");
+        System.out.println("* * * * * * ======================================");
+        System.out.println(" * * * * * =======================================");
+        System.out.println("* * * * * * ======================================");
+        System.out.println(" * * * * * =======================================");
+        System.out.println("* * * * * * ======================================");
+        System.out.println("==================================================");
+        System.out.println("==================================================");
+        System.out.println("==================================================");
+        System.out.println("==================================================");
+        System.out.println("==================================================");
+        System.out.println("==================================================");
+        System.out.println();
+
+//Question 7
+    System.out.println("Question 7");
+    val1 = 74;
+    val2 = 36;
+
+    total = val1 + val2;
+    System.out.println("Total Value = " + total);
+        System.out.println();
+
+//Question 8
+System.out.println("Question 8");
+val1 = 50;
+val2 = 3;
+
+total = val1/val2;
+System.out.println(total);
+
+//Question 9
+System.out.println("Question 9");
+total = -5 + 8 * 6;
+System.out.println("Question a: " + total);
+
+total = (55+9)%9;
+System.out.println("Question b: " + total);
+
+total = 20+(-3)*5/8;
+System.out.println("Question c: " + total);
+
+total = 5+15/3*2-8%3;
+System.out.println("Question d: " + total);
+System.out.println();
+
+
+//Question 10
+System.out.println("Question 10");
+val1 = 25;
+val2 = 5;
+total = val1 * val2;
+System.out.println("25 x 5 = " + total);
+System.out.println();
+
+//Question 11
+        System.out.println("Question 12");
+
+        System.out.println("1 x 8 = " + 1*8);
+        System.out.println("1 x 8 = " + 2*8);
+        System.out.println("1 x 8 = " + 3*8);
+        System.out.println("1 x 8 = " + 4*8);
+        System.out.println("1 x 8 = " + 5*8);
+        System.out.println("1 x 8 = " + 6*8);
+        System.out.println("1 x 8 = " + 7*8);
+        System.out.println("1 x 8 = " + 8*8);
+        System.out.println("1 x 8 = " + 9*8);
+        System.out.println("1 x 8 = " + 10*8);
+
+//Question 12
+        System.out.println("Question 12");
+        System.out.println("  J  a v  v a");
+        System.out.println("  J a a v v a a");
+        System.out.println("J J aaaaa V V aaaaa");
+        System.out.println(" JJ a  a V a   a");
+
+//Question 13
+        System.out.println("Question 13");
+        System.out.println("Java Version: 1.8.0_71");
+        System.out.println("Java Runtime Version: 1.8.0_71-b15");
+        System.out.println("Java Home: /opt/jdk/jdk1.8.0_71/jre");
+        System.out.println("Java Vendor: Oracle Corporation");
+        System.out.println("Java Vendor URL: http://Java.oracle.com/");
+        System.out.println("Java Class Path: .");
+
+//Question 14
+        System.out.println("Question 14");
+        System.out.println("25 != 39");
+        System.out.println("25 < 39");
+        System.out.println("25 <= 39");
+
+//Question 15
+        text = "Aa kiu, I swd skieo 236587. GH kiu: sieo?? 25.33";
+        System.out.println("Question 15");
+        System.out.println(text);
+
+//Question 16
+        doubleVal1 = ((25.5 * 3.5 - 3.5 * 3.5) / (40.5 - 4.5));
+        System.out.println("Question 16");
+        System.out.println(doubleVal1);
+
+//Question 17
+        System.out.println("Question 17");
+        text = "Twinkle, twinkle, little star,";
+        System.out.println(text);
+        text ="       How I wonder what you are!";
+        System.out.println(text);
+        text ="             Up above the world so high,";
+        System.out.println(text);
+        text ="                Like a diamond in the sky.";
+        System.out.println(text);
+        text ="Twinkle, twinkle, little star,";
+        System.out.println(text);
+        text ="      How I wonder what you are";
+        System.out.println(text);
+*/
+//Question 18
+        System.out.println("Question 18");
+        text = "Denish";
+        String subject = "maths";
+        doubleVal1 = 78.8;
+        double passMark = 80;
+
+        if (doubleVal1 >= passMark) {
+            System.out.println("Congratulation " + text + " you have passed " + subject +
+                    "with a mark of " + doubleVal1 + ", the pass mark for this subject was " + passMark);
+        }else{
+            System.out.println("Unfortunatley " + text + " you have not passed " + subject + " the pass mark for " +
+                    subject + " is " + passMark + " and your mark was " + doubleVal1);
+            }
+        System.out.println();
+
+        text = "Mike";
+        subject = "Maths";
+        doubleVal1 = 85.9;
+
+        if (doubleVal1 >= passMark) {
+            System.out.println("Congratulation " + text + " you have passed " + subject +
+                    "with a mark of " + doubleVal1 + ", the pass mark for this subject was " + passMark);
+        }else{
+            System.out.println("Unfortunatley " + text + " you have not passed " + subject + " the pass mark for " +
+                    subject + " is " + passMark + " and your mark was " + doubleVal1);
+        }
+        System.out.println();
+
+        text = "Alice";
+        subject = "French";
+        doubleVal1 = 85.0;
+        passMark = 85;
+
+        if (doubleVal1 >= passMark) {
+            System.out.println("Congratulation " + text + " you have passed " + subject +
+                    "with a mark of " + doubleVal1 + ", the pass mark for this subject was " + passMark);
+        }else{
+            System.out.println("Unfortunatley " + text + " you have not passed " + subject + " the pass mark for " +
+                    subject + " is " + passMark + " and your mark was " + doubleVal1);
+        }
+        System.out.println();
+
+        text = "Grace";
+        subject = "French";
+        doubleVal1 = 83.8;
+        passMark = 85;
+
+        if (doubleVal1 >= passMark) {
+            System.out.println("Congratulation " + text + " you have passed " + subject +
+                    "with a mark of " + doubleVal1 + ", the pass mark for this subject was " + passMark);
+        }else{
+            System.out.println("Unfortunatley " + text + " you have not passed " + subject + " the pass mark for " +
+                    subject + " is " + passMark + " and your mark was " + doubleVal1);
+        }
+
+
+            /*
+//Question 19
+
+        val1 = 17;
+        doubleVal1 = 3.14;
+        text = "Hi";
+         System.out.println("Question 19");
+        System.out.println("My int value is: " + val1);
+        System.out.println("My double value is: " + doubleVal1);
+        System.out.println("My char value is: " + text);
+
+//Question 20
+        val1 = 17;
+        doubleVal1 = 5.2;
+
+        double doubletotal = val1 + doubleVal1;
+
+        System.out.println("Question 20");
+        System.out.println("The sum of " + val1 + " and " + doubleVal1 + " is: " + doubletotal);
+
+*/
+    }
+}
